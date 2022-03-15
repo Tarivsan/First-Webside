df69e2dee1ecf638a82ee3c745e99d61de1e839c
Damian Grzesiak <damiann.grzesiakk@gmail.com>
2022-03-15 09:35:24 +0100

    config update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://tariv:P9N1H3j3FqKTtb4Z@cluster0.fzlvx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     keysSession: ['twój klucz'],
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
 }
